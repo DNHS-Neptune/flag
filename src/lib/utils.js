309a3ddc66411fd36479a05060f5ab92e03cd22b
Nolan Hightower <oioioihomelander@yahoo.com>
2024-12-18 23:43:43 -0800

    broken code :(

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,25 +1,20 @@
 import { browser } from '$app/environment';
 import { base } from '$app/paths';
+import countries from 'i18n-iso-countries';
 
 function randomize(array) {
 	return array.sort(() => Math.random() - 0.5);
 }
 
-function nameToPath(name) {
-	return `/${base}/f/${name.toLowerCase().replace(/ /g, '_').replace(/'/g, '')}`;
+function codeToName(code) {
+	return countries.getName(code, 'en');
 }
 
-const cached = [];
-function cacheImage(name) {
-	return new Promise((resolve, reject) => {
-		if (browser) {
-			const image = new Image();
-			image.src = nameToPath(name);
-			image.onload = () => resolve(image);
-			image.onerror = (error) => reject(error);
-			cached.push(image);
-		}
-	});
+function cacheImage(code) {
+	if (browser) {
+		const img = new Image();
+		img.src = `/${base}/f/png1000px/${code.toLowerCase()}.png`;
+	}
 }
 
-export { randomize, nameToPath, cacheImage };
+export { randomize, codeToName, cacheImage };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
+<script>
+	import { randomize, codeToName, cacheImage } from '$lib/utils';
+	let arr = [1, 2, 3];
+	console.log(randomize(arr));
+</script>
+
 <h1>flag</h1>
